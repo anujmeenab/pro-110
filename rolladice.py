f85713fcf290d5fd2b54cf9d9edd086920b82c91
anujmeenab <105282094+anujmeenab@users.noreply.github.com>
2022-11-11 20:03:40 +0530

    Add files via upload

---
+++ rolladice.py
@@ -0,0 +1,45 @@
+import random
+response="y"
+while response=="y":
+    no =random.randint(1,6)
+    if no==1:
+        print("[////]")
+        print("[0]")
+        print("[----]")
+        print("[++++]")
+        print("[****]")
+    if no==2:
+        print("[!!!!]")
+        print("[00]")
+        print("[@@@@]")
+        print("[####]")
+        print("[$$$$")
+    if no==3:
+        print("[%%%%]")
+        print("[^^^^]")
+        print("[&&&&]")
+        print("[ ]")
+        print("[,,,,]")
+    if no==4:
+        print("[;;;;]")
+        print("[????]")
+        print("[>>>>]")
+        print("[++++++]")
+        print("[******]")
+    if no==5:
+        print("[//////]")
+        print("[00000]")
+        print("[------]")
+        print("[++++++]")
+        print("[******]")
+    if no==6:
+        print("[//////]")
+        print("[000000]")
+        print("[------]")
+        print("[++++++]")
+        print("[******]")
+    response=input("y to continue and n to exit")
+    print("\n")
+
+    
+
